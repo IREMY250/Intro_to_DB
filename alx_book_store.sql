41844b7a6f020f016ca556ffba2927e95922d5d8
IREMY250 <ibyishakajeanremy0@gmail.com>
2024-07-26 22:03:43 +0200

    ALX BOOK STORE

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,27 +1,34 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 CREATE TABLE Authors (
-     author_id(primary key),
-     author_name VARCHAR(215)
+     author_id INT (primary key),
+     author_name VARCHAR(215) NOT NULL
      );
 CREATE TABLE Books (
-    book_id (Primary Key),
-     title VARCHAR(130),
-     author_id,
-     price DOUBLE,
+    book_id INT (Primary Key),
+     title VARCHAR(130) NOT NULL,
+     author_id INT,
+     price DOUBLE NOT NULL,
      publication_date DATE
+     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
      );
 CREATE TABLE Customers (
     customer_id(primary key),
-     customer_name VARCHAR(215),
-     email VARCHAR (215),
+     customer_name VARCHAR(215) NOT NULL,
+     email VARCHAR (215) NOT NULL,
      address TEXT
      );
 CREATE TABLE Orders (
     order_id (Primary Key),
-     customer_id,order_date DATE);
+     customer_id,
+     order_date DATE
+     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+     );
 CREATE TABLE Order_Details (
     orderdetail_id (Primary Key),
-    order_id,book_id,
+    order_id,
+    book_id,
     quantity DOUBLE
+     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+     FOREIGN KEY (book_id) REFERENCES Books(book_id)
     );
