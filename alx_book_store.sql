b5105006fd4009985d63d487d478853d539df8a9
IREMY250 <ibyishakajeanremy0@gmail.com>
2024-07-26 21:39:35 +0200

    ALX BOOK STORE

---
+++ alx_book_store.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Books (book_id (Primary Key), title VARCHAR(130),author_id,price DOUBLE,publication_date DATE);
+CREATE TABLE Authors (author_id(primary key),author_name VARCHAR(215));
+CREATE TABLE Customers (customer_id(primary key),customer_name VARCHAR(215),email VARCHAR (215),address TEXT);
+CREATE TABLE Orders (order_id (Primary Key),customer_id,order_date DATE);
+CREATE TABLE Order_Details (orderdetail_id (Primary Key),order_id,book_id,quantity DOUBLE);
